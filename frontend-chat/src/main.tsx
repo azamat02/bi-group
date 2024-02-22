449b095d95e6f60741ef49d8322e0cd034148298
saiduly <azamattolegenov1@gmail.com>
2024-02-22 16:36:38 +0600

    Added call button

--- frontend-chat/src/main.tsx
+++ frontend-chat/src/main.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const CallComponent = () => {
+    useEffect(() => {
+        window.location.href = 'tel:1360'
+    }, []);
+
+    return <>
+    </>
+}
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: '/call',
+        element: <CallComponent/>
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
