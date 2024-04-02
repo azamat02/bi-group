8c33f4c0ebbf68afdcebe816f21f316573f11877
saiduly <azamattolegenov1@gmail.com>
2024-04-02 15:57:35 +0500

    bot change

--- frontend-chat/src/main.tsx
+++ frontend-chat/src/main.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect} from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
